5533c2dd88b5aac9580960940facf4b764b4daab
Eden Ghirmai <edeng94@gmail.com>
2024-05-04 11:16:56 -0700

    nextjs stuff

--- src/app/authentication/login/page.tsx
+++ src/app/authentication/login/page.tsx
@@ -55,7 +55,7 @@ const Login2 = () => {
                     color="textSecondary"
                     mb={1}
                   >
-                    Your Social Campaigns
+                    Your pro-forma models
                   </Typography>
                 }
                 subtitle={
@@ -70,7 +70,7 @@ const Login2 = () => {
                       variant="h6"
                       fontWeight="500"
                     >
-                      New to Spike?
+                      New to Fortell?
                     </Typography>
                     <Typography
                       component={Link}
